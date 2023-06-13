e4b0a49968a4e54e712f8284abca0efb52ed9390
Robert YC Park <robertpark1228@gmail.com>
2023-06-13 13:31:18 +0900

    Update sed_sample.sh

--- sed_sample.sh
+++ sed_sample.sh
@@ -1,1 +1,1 @@
-while read line;do n=$(echo ${line} | cut -d "," -f1);c=$(echo ${line} | cut -d "," -f2);echo sed \"{s/${n}\,/${c}\,/g}\";done < listup_samples | sed -z 's/\n/ | /g'
+while read line;do n=$(echo ${line} | cut -d "," -f1);c=$(echo ${line} | cut -d "," -f2);echo sed \"{s/${n}\,/${c}\,/g}\" | sed \"{s/${n}$/${c}$/g}\";done < listup_samples | sed -z 's/\n/ | /g'
