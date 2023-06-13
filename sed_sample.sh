8c1393c81b3fb859f3a5a077a17fbde020fcebe4
Robert YC Park <robertpark1228@gmail.com>
2023-06-13 17:07:09 +0900

    Update sed_sample.sh

--- sed_sample.sh
+++ sed_sample.sh
@@ -1,1 +1,1 @@
-while read line;do n=$(echo ${line} | cut -d "," -f1);c=$(echo ${line} | cut -d "," -f2);echo sed \"{s/${n}\,/${c}\,/g}\";echo sed \"{s/${n}\$/${c}\$/g}\";done< sampleout1.csv | sed -z 's/\n/ | /g'
+while read line;do n=$(echo ${line} | cut -d "," -f1);c=$(echo ${line} | cut -d "," -f2);echo sed \"{s/${n}\,/${c}\,/g}\";echo sed \"{s/${n}\$/${c}\/g}\";done< dead_alive.csv | sed -z 's/\n/ | /g'
